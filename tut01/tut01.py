33ae8916e53ce74bd8ef1809f8335b40be944138
Aditya01112000 <72222830+Aditya01112000@users.noreply.github.com>
2021-08-18 22:34:31 +0530

    Assignment

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,34 @@
-def meraki_helper(n):
-    """This will detect meraki numner"""
+# defining function to check whether the given number present in the list pass to the function is meraki number or not 
+def MERAKI(n):
+        f=0
+            # finding the absolute difference between the number and its adjacent
+        for j in range(len(str(n))-1):
+            n=str(n)
+                
+            if abs(int(n[j])-int(n[j+1]))==1:
+                f=0
+            else:
+                f=1
+                break
 
+        # printing yes / no based on the flag value
+        if f==0:
+            print("Yes,", n, "is a Meraki number.")
+            return True
+        else:
+            print("No,", n, "is not a Meraki number.")
+            return False
 
-input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+input = [int(item) for item in input("Enter the input : ").split()] 
+# input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+
+cnt_yes = 0
+cnt_no = 0 
+# iterating through the given input
+for n in input:
+    if MERAKI(n):
+        cnt_yes += 1
+    else:
+        cnt_no += 1
+    
+print("The input list contains", cnt_yes,"meraki and", cnt_no,"non meraki numbers.")
